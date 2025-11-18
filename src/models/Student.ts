1930a3bd7f9f042f9ea0d698e3570d2ce9bac354
Jedidiah Paul <violater914@gmail.com>
2025-11-18 23:02:09 +0530

    first commit

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const StudentSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    section: { type: String, enum: ["A", "B", "C", "D"], required: true },
     email: { type: String, required: true },
     dob: { type: Date, required: true },
     gender: { type: String, enum: ["male", "female"], required: true },
