f2efff0b290172a682f06a834b77b868690b4520
Jedidiah Paul <violater914@gmail.com>
2025-11-19 00:12:12 +0530

    first commit

--- src/routes/api/students/index.ts
+++ src/routes/api/students/index.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import { Student } from "../../../models/Student";
 
 export const GET = async (req: Request, res: Response) => {
-  const students = await Student.find();
-  res.json(students);
+    const students = await Student.find();
+    res.json(students);
 };
 
 export const POST = async (req: Request, res: Response) => {
-  const student = await Student.create(req.body);
-  res.json(student);
+    const student = await Student.create(req.body);
+    res.json(student);
 };
